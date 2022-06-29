2ca6085a794380baef167f308df4fc4796f055bd
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2022-06-29 12:04:42 +0200

    added html structure

--- calculator.js
+++ calculator.js
@@ -14,5 +14,15 @@ function multiply(a, b) {
   return a * b;
 }
 
+
+// STEPS TO RUN THE TEST:
+// - open your terminal in the location of the lab
+// - run this only once: npm i 
+// - run this only once: npm run test:watch
+// - run live server on lab-solution.html
+// - the tests will be automatically updated everytime we code and save the js file
+
 // The following is required to make unit tests work. Please ignore it.
-module.exports = { sum, subtract, divide, multiply };
+if (typeof module !== 'undefined') {
+  module.exports = { sum, subtract, divide, multiply };
+}
